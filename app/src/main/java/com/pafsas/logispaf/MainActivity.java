ce06f63c8397b0543a70d186df6d05df9109020c
sovdlp <88692839+sovdlp@users.noreply.github.com>
2022-01-26 17:09:08 -0500

    splash screen test 26-01-22 1700hr

--- app/src/main/java/com/pafsas/logispaf/MainActivity.java
+++ app/src/main/java/com/pafsas/logispaf/MainActivity.java
@@ -2,13 +2,30 @@ package com.pafsas.logispaf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 10000);
     }
 }
